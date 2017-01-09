c1d3b65c1df322971379b32c55c1e63fb0d0d810
Henry Mortimer <henry@morti.plus.com>
2017-01-09 15:41:42 +0000

    implemented basic DVR

--- DV.java
+++ DV.java
@@ -1,76 +1,154 @@
 import java.lang.Math;
+import java.util.HashMap;
+import java.util.Formatter;
+import java.util.Vector;
 
 public class DV implements RoutingAlgorithm {
     
     static int LOCAL = -1;
     static int UNKNOWN = -2;
     static int INFINITY = 60;
-    
+
+    private HashMap<Integer, DVRoutingTableEntry> routingTable;
+
+    private boolean shpr;
+    private boolean expr;
+
+    private int uInterval;
+
+    private Router router;
+
     public DV()
     {
+      routingTable = new HashMap<Integer, DVRoutingTableEntry>();
     }
     
     public void setRouterObject(Router obj)
     {
+      router = obj;
     }
     
     public void setUpdateInterval(int u)
     {
+      uInterval = u;
     }
     
     public void setAllowPReverse(boolean flag)
     {
+      shpr = flag;
     }
     
     public void setAllowExpire(boolean flag)
     {
+      expr = flag;
     }
     
     public void initalise()
     {
+      int d = router.getId();
+      DVRoutingTableEntry entry = new DVRoutingTableEntry(d, LOCAL, 0, INFINITY);
+      routingTable.put(d, entry);
     }
     
     public int getNextHop(int destination)
     {
-        return 0;
+      DVRoutingTableEntry entry = routingTable.get(destination);
+      return entry.getInterface();
     }
     
     public void tidyTable()
     {
+      for(DVRoutingTableEntry entry:routingTable.values()){
+        int i = entry.getInterface();
+        if(!router.getInterfaceState(i)){
+          entry.setMetric(INFINITY);
+        }
+      }
     }
     
     public Packet generateRoutingPacket(int iface)
     {
+      if(router.getInterfaceState(iface)){
+        Payload p = new Payload();
+        for(DVRoutingTableEntry e:routingTable.values()){
+          DVRoutingTableEntry newe = new DVRoutingTableEntry(e.getDestination(),
+                                                             e.getInterface(),
+                                                             e.getMetric(),
+                                                             e.getTime());
+          p.addEntry(newe);
+        }
+        Packet pack = new RoutingPacket(router.getId(), Packet.BROADCAST);
+        pack.setPayload(p);
+        return pack; 
+      } else {
         return null;
+      }
+    }
+
+    private void createEntry(DVRoutingTableEntry entry, int iface){
+      int d = entry.getDestination();
+      int m = entry.getMetric();
+      m += router.getInterfaceWeight(iface);
+      if(m > INFINITY){
+        m = INFINITY;
+      }
+      DVRoutingTableEntry r = routingTable.get(d);
+      if(r == null){
+        DVRoutingTableEntry newr = new DVRoutingTableEntry(d, iface, m, INFINITY);
+        routingTable.put(d, newr);
+      } else if (iface == r.getInterface()){
+        r.setMetric(m);
+      } else if (m < r.getMetric()) {
+        r.setMetric(m);
+        r.setInterface(iface);
+      }
     }
     
     public void processRoutingPacket(Packet p, int iface)
     {
+      Vector<Object> d = p.getPayload().getData();
+      for(Object entry: d){
+        createEntry((DVRoutingTableEntry)entry, iface);
+      } 
     }
     
     public void showRoutes()
     {
+       System.out.format("Router %d%n", router.getId());
+       for(DVRoutingTableEntry entry : routingTable.values()){
+         System.out.println(entry);
+       }
     }
 }
 
 class DVRoutingTableEntry implements RoutingTableEntry
 {
+  private int destination;
+  private int iface;
+  private int metric;
+  private int time;
     
     public DVRoutingTableEntry(int d, int i, int m, int t)
 	{
+    destination = d;
+    iface = i;
+    metric = m;
+    time = t;
 	}
-    public int getDestination() { return 0; } 
-    public void setDestination(int d) { }
-    public int getInterface() { return 0; }
-    public void setInterface(int i) { }
-    public int getMetric() { return 0;}
-    public void setMetric(int m) { } 
-    public int getTime() {return 0;}
-    public void setTime(int t) { }
+    public int getDestination() { return destination; } 
+    public void setDestination(int d) { destination = d;}
+    public int getInterface() { return iface; }
+    public void setInterface(int i) { iface = i;}
+    public int getMetric() { return metric;}
+    public void setMetric(int m) { metric = m;} 
+    public int getTime() {return time;}
+    public void setTime(int t) { time = t;}
     
     public String toString() 
 	{
-	    return "";
+    Formatter formatter = new Formatter();
+    formatter.format("d %d i %d m %d", destination, iface, metric);
+    return formatter.toString();
 	}
 }
 
